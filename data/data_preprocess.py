2c30070f776c8d0aca0e788e48a78317cb50d7fb
evan <739228954@qq.com>
2020-07-29 23:39:16 +0800

    add data_preprocess

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -1,6 +1,9 @@
 # --*--coding: utf-8 --*--
 """
-
+@Author              :    谋杀电视机
+@Contact            :    739228954@qq.com
+@DateTime        :    2020-07
+@Desc                  :    该code主要用来处理aclmdb数据及nlp7294,具体数据介绍见README.md
 """
 import os
 
@@ -9,43 +12,51 @@ class AclImdb_processing():
 
     def __init__(self, dirname):
         self.root = dirname
-        self.train_dirname = os.path.join(dirname, 'train')
-        self.test_dirname = os.path.join(dirname, 'test')
-        self.file_merge(self.train_dirname, 'train')
-        self.file_merge(self.test_dirname, 'test')
+        self.train_dirname = os.path.join(dirname, 'train')  # train数据的地址
+        self.test_dirname = os.path.join(dirname, 'test')  # test数据地址
+        self.file_merge(self.train_dirname, 'train')  # 将train数据进行合并
+        self.file_merge(self.test_dirname, 'test')  # 将test数据进行合并
 
     def file_merge(self, dir, result_name):
+        """将aclimdb数据文件进行整合,生成2个文件,分别是test/train-pos/neg
+
+        Args:
+            dir: String 即目录地址;
+            result_name: String 即train或test文件夹;
+        Return:
+            None;
+        """
         all_files = {}
         for root, dirs, files in os.walk(dir, topdown=False):
             self.dirs = dirs  # 子目录
-            all_files["all"] = dirs
+            all_files["all"] = dirs  # [pos,neg unsup]
         for i in range(len(self.dirs)):
             for _, _, files in os.walk(os.path.join(dir, self.dirs[i]), topdown=False):
-                all_files[self.dirs[i]] = files  # 将pos,neg文件名称汇总
+                all_files[self.dirs[i]] = files  # 将pos,neg unsup文件名称汇总
 
         if result_name == 'train':
-            pos_file = os.path.join(self.root, "train_pos.txt")
-            neg_file = os.path.join(self.root, "train_neg.txt")
+            file = os.path.join(self.root, "train.txt")
+            #neg_file = os.path.join(self.root, "train_neg.txt")
         else:
-            pos_file = os.path.join(self.root, "test_pos.txt")
-            neg_file = os.path.join(self.root, "test_neg.txt")
+            file = os.path.join(self.root, "test.txt")
+            #neg_file = os.path.join(self.root, "test_neg.txt")
 
-        with open(pos_file, "w+", encoding='utf8') as f:
+        with open(file, "w+", encoding='utf8') as f:
             num = 0
-            for i in all_files["pos"]:
+            for i in all_files["pos"]:  # 取pos文件夹下的文件
                 num += 1
                 if num % 100 == 0:
                     print(result_name, ' pos:', num)
-                with open(os.path.join(os.path.join(dir, "pos"), i), encoding="utf8") as fe:
-                    f.write(fe.read()+"\n")  # 写入pos总文件中
-        with open(neg_file, "w+", encoding='utf8') as f:
+                with open(os.path.join(os.path.join(dir, "pos"), i), encoding="utf8") as fe:  # 打开需要读取的文件
+                    f.write(fe.read()+"\t"+"1"+"\n")  # 写入pos总文件中 每个文件进行换行
+        #with open(neg_file, "w+", encoding='utf8') as f:
             num = 0
             for i in all_files["neg"]:
                 num += 1
                 if num % 100 == 0:
                     print(result_name, ' neg:', num)
                 with open(os.path.join(os.path.join(dir, "neg"), i), encoding="utf8") as fe:
-                    f.write(fe.read()+"\n")
+                    f.write(fe.read()+"\t"+"0"+"\n")
 
 
 if __name__ == "__main__":
